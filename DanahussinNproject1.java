b64a8358eee9ca8d660919507345b78731676747
danahsin_ <danarhusain@gmail.com>
2025-08-30 00:17:12 +0700

    Delete DanahussinNproject1.java

--- DanahussinNproject1.java
+++
@@ -1,9 +0,0 @@
-public class DanahussinNproject1 {
-    public static void main(String[] args) {
-        System.out.println("Hallo perkenalkan");
-        System.out.println("Nama    : Widyadana Hussin Firmansyah");
-        System.out.println("NPM     : 2440506054");
-        System.out.println("Alamat  : Jalan Urip Sumoharjo, Wates Prontaan");
-        System.out.println("No_HP   : 08-sekian");
-    }
-}
